7feb76e36e8a53a76e54c2f41fb5901eda5c5780
alysom <alysom.3d@live.com>
2022-12-26 21:32:51 +0000

    miniprojetos

--- lampada.js
+++ lampada.js
@@ -7,7 +7,7 @@ lamp.addEventListener("mouseover", lampOn);
 lamp.addEventListener("mouseleave", lampOff);
 lamp.addEventListener("dblclick", lampQuebra)
 
-//cria uma funcao que troca o nome do botao e liga e desliga a lampada
+
 function lampOnOff (){
     if ( turnOnOff.textContent == "Ligar"){
         lampOn();
@@ -18,12 +18,12 @@ function lampOnOff (){
     }
 
 }
-//cria uma funcao que busca dentro de lamp uma string com nome "quebrada" neste caso a string quebrada é o nome de imagem,quando encontra tras um numero maior que -1 e quando nao encontra tras -1
+
 function isLampBroken () {
     return lamp.src.indexOf ("quebrada") > -1;
 }
 
-// quando a lampada NAO estiver quebrada (!isLampBroken) ele executa a funcao
+
 function lampOn (){
     if (!isLampBroken () ){
       lamp.src = "./img/ligada.jpg";  
